7d57386b560dc4d96eec50485b3d97d7d2a5ac1b
saitarun135 <saitarun135@gm.com>
2021-04-25 20:15:01 +0530

    sai[Added]:calculate employee wages

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,10 +1,13 @@
 <?php
 $IS_FULL_TIME=1;
+$EMP_RATE_PER_HR=20;
 $empChk=rand(1,2);
-    if($empChk==$IS_FULL_TIME){
-        echo "<h2>Employee is present</h2>";
-    }
-    else{
-        echo "Employee is Absent";
-    }
+  if($empChk==$IS_FULL_TIME){
+      $empHrs=8;
+  }
+  else{
+      $empHrs=0;
+  }
+  $empWage=$empHrs * $EMP_RATE_PER_HR;
+  echo "Employee Wage is:".$empWage;
 ?>
