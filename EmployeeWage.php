377bc3aada4fab6b5a4ac6b6e7c5b52daac088a6
saitarun135 <saitarun135@gm.com>
2021-04-25 20:12:28 +0530

    [sai]Added:checking employee present or absent

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,3 +1,10 @@
 <?php
-echo "Welcome to the Employee-wage-problem";
+$IS_FULL_TIME=1;
+$empChk=rand(1,2);
+    if($empChk==$IS_FULL_TIME){
+        echo "<h2>Employee is present</h2>";
+    }
+    else{
+        echo "Employee is Absent";
+    }
 ?>
