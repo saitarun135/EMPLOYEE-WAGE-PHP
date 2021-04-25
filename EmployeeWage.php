161781abe76cfd5af21d64481b955d52f8fd8488
saitarun135 <saitarun135@gm.com>
2021-04-25 20:17:23 +0530

    [sai]Added:adding parttime employeeWage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,9 +1,14 @@
 <?php
-$IS_FULL_TIME=1;
+$IS_PART_TIME=1;
+$IS_FULL_TIME=2;
 $EMP_RATE_PER_HR=20;
-$empChk=rand(1,2);
+$empChk=rand(1,3);
+
   if($empChk==$IS_FULL_TIME){
       $empHrs=8;
+  }
+  elseif($empChk==$IS_PART_TIME){
+      $empHrs=4;
   }
   else{
       $empHrs=0;
