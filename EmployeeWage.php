bb738bae288a4e99cfde36b620e3f3e8939690ab
saitarun135 <saitarun135@gm.com>
2021-04-25 20:32:21 +0530

    [sai]Added:refactoring the code

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,32 +1,42 @@
 <?php
-$IS_PART_TIME=1;
-$IS_FULL_TIME=2;
-$EMP_RATE_PER_HR=20;
-$totalEmpWage=0;
-$NUM_OF_WORKING_DAYS=24;
-$MAX_HRS_IN_MONTH=120;
-$totalEmpHrs=0;
-$totalWorkingDays=0;
-$empHrs=0;
-    while($totalEmpHrs<=$MAX_HRS_IN_MONTH && 
-        $totalWorkingDays<$NUM_OF_WORKING_DAYS){
-    $totalWorkingDays++;
-    $empChk=rand(1,3);
-    switch($empChk){
-        case $IS_PART_TIME:
-                $empHrs=4;
-                break;
-        case $IS_FULL_TIME:
-                $empHrs=8;
-                break;
-        default:
-            $empHrs=0;
-            break;
-    }
+class EmpWageBuilder{
+    const IS_PART_TIME=1;
+    const IS_FULL_TIME=2;
+    const EMP_RATE_PER_HOUR=18;
+    const NO_OF_WORKING_DAYS=24;
+    const MAX_HRS_IN_MONTH=120;
 
-    $totalEmpHrs += $empHrs;
-    echo "Days:".$totalWorkingDays."Hours:".$empHrs."<br>";  
+function computeEmployeeWage(){
+    $empHrs=0;
+    $totalEmpHrs=0;
+    $totalWorkingDays=0;
+    while($totalEmpHrs<=(self::MAX_HRS_IN_MONTH) && $totalWorkingDays<(self::NO_OF_WORKING_DAYS)){
+            $totalWorkingDays++;
+            $empChk=rand(1,2);
+            switch($empChk){
+                case 1:
+                    $empHrs=4;
+                    break;
+                case (self::IS_FULL_TIME):
+                    $empHrs=8;
+                    break;
+                default:
+                    $empHrs=0;
+                    break;
+            }
+            $totalEmpHrs += $empHrs;
+            echo "Day".$totalWorkingDays."EmployeeHrs:".$empHrs."<br>";
+           
+            
+    }
+    $totalEmpWage=$totalEmpHrs * (self::EMP_RATE_PER_HOUR);
+    echo "TotalEmployeeHours:".$totalEmpHrs.'<br>';
+    echo "TotalEmployeeWage: ".$totalEmpWage."<br>";
+    return $totalEmpWage;
+  
+    }
 }
-$totalEmpWage=$totalEmpHrs * $EMP_RATE_PER_HR;
-echo "Total Empwage:".$totalEmpWage;
+
+$calculateWage=new EmpWageBuilder;
+$calculateWage->computeEmployeeWage();
 ?>
