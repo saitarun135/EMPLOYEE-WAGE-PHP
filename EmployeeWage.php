3d2e3f8fadc7e0d80f28dcab73d2094fbcae7a04
saitarun135 <saitarun135@gm.com>
2021-04-25 20:28:18 +0530

    [sai]Added:calculating employeewage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -3,28 +3,30 @@ $IS_PART_TIME=1;
 $IS_FULL_TIME=2;
 $EMP_RATE_PER_HR=20;
 $totalEmpWage=0;
-$NUM_OF_WORKING_DAYS=2;
-for($day=0;$day<$NUM_OF_WORKING_DAYS;$day++){
+$NUM_OF_WORKING_DAYS=24;
+$MAX_HRS_IN_MONTH=120;
+$totalEmpHrs=0;
+$totalWorkingDays=0;
+$empHrs=0;
+    while($totalEmpHrs<=$MAX_HRS_IN_MONTH && 
+        $totalWorkingDays<$NUM_OF_WORKING_DAYS){
+    $totalWorkingDays++;
     $empChk=rand(1,3);
     switch($empChk){
-        case 1:
-            $empChk==$IS_PART_TIME;
-            $empHrs=4;
-            break;
-        case 2:
-            $empChk== $IS_FULL_TIME;
-            $empHrs=8;
-            break;    
+        case $IS_PART_TIME:
+                $empHrs=4;
+                break;
+        case $IS_FULL_TIME:
+                $empHrs=8;
+                break;
         default:
             $empHrs=0;
             break;
-        }
-        $empWage=$EMP_RATE_PER_HR*$empChk;
-        $totalEmpWage += $empWage;
-       // echo "Employee wage:".$empWage."\n";
-       
-}
-echo "Total wage :".$totalEmpWage."<br>";
-echo "Employee wage:".$empWage;
+    }
 
+    $totalEmpHrs += $empHrs;
+    echo "Days:".$totalWorkingDays."Hours:".$empHrs."<br>";  
+}
+$totalEmpWage=$totalEmpHrs * $EMP_RATE_PER_HR;
+echo "Total Empwage:".$totalEmpWage;
 ?>
