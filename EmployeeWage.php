91a7e41b935f9c8351e05834734e38a1d37b88b0
saitarun135 <saitarun135@gm.com>
2021-04-25 20:20:12 +0530

    [sai]Added:using switch case statement

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,18 +1,24 @@
 <?php
-$IS_PART_TIME=1;
-$IS_FULL_TIME=2;
-$EMP_RATE_PER_HR=20;
-$empChk=rand(1,3);
-
-  if($empChk==$IS_FULL_TIME){
-      $empHrs=8;
-  }
-  elseif($empChk==$IS_PART_TIME){
-      $empHrs=4;
-  }
-  else{
-      $empHrs=0;
-  }
-  $empWage=$empHrs * $EMP_RATE_PER_HR;
-  echo "Employee Wage is:".$empWage;
+function calWage(){
+    $IS_PART_TIME=1;
+    $IS_FULL_TIME=2;
+    $EMP_RATE_PER_HR=20;
+    $empChk=rand(1,3);
+    switch($empChk){
+        case 1:
+            $empChk==$IS_PART_TIME;
+            $empHrs=4;
+            break;
+        case 2:
+            $empChk== $IS_FULL_TIME;
+            $empHrs=8;
+            break;    
+        default:
+            $empHrs=0;
+            break;
+        }
+        $empWage=$EMP_RATE_PER_HR*$empChk;
+        echo "Employee wage:".$empWage;
+}
+  calWage();
 ?>
